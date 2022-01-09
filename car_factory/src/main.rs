310b14f37054d5416075a6e51b70925cf3aeefbf
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-01-09 23:30:28 +0300

    Add types sample

---
+++ car_factory/src/main.rs
@@ -0,0 +1,39 @@
+
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+enum Transmission {
+    Automatic,
+    SemiAuto,
+    Manual,
+}
+
+// Build a "Car" by using values from the input arguments
+// - Color of car (String)
+// - Transmission type (enum value)
+// - Convertible (boolean, true if car is a convertible)
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    return Car {
+        color: color,
+        transmission: transmission,
+        convertible: convertible,
+        mileage: 0,
+    };
+}
+
+fn main() {
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+}

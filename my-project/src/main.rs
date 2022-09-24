cf3cc2389259d688ef08c71d7f9674b571ac3c42
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2022-09-25 00:38:37 +0300

    Use 3rd-party dependency

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,8 +1,13 @@
 mod authentication;
 
+use regex::Regex;
+
 fn main() {
     let mut user = authentication::User::new("jeremy", "super-secret");
 
     println!("The username is: {}", user.get_username());
     user.set_password("even-more-secret");
+
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
